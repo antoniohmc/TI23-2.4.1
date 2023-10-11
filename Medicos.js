8dcae6db8211e69df7f60e43dcd974551b5541e8
Antonio Campos <112445736+antoniohmc@users.noreply.github.com>
2023-10-11 18:47:36 -0300

    Add files via upload

---
+++ Medicos.js
@@ -0,0 +1,21 @@
+const medicos = [
+    {
+      nome: 'Leticia',
+      sobrenome: 'Costana',
+      genero: 'feminino',
+      imagemPerfil: 'http://placekitten.com/400/400'
+    },
+    {
+      nome: 'Adimaldo',
+      sobrenome: 'Pinto',
+      genero: 'masculino',
+      imagemPerfil: 'http://placekitten.com/401/401'
+    },
+    {
+      nome: 'Noah',
+      sobrenome: 'Lutus',
+      genero: 'nao-binario',
+      imagemPerfil: 'http://placekitten.com/402/402'
+    },
+  ];
+  
